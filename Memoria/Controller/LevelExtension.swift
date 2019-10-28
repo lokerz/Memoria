1293b79f67de577d508e47999e834eaca427e557
Ridwan Abdurrasyid <ridwan.h.abdurrasyid@gmail.com>
2019-10-28 14:51:08 +0700

    alpha, camera and light fixed

--- Memoria/Controller/LevelExtension.swift
+++ Memoria/Controller/LevelExtension.swift
@@ -9,13 +9,30 @@
 import Foundation
 import SceneKit
 
-enum relation {
-    case sameDirection
-    case oppositeDirection
-    case nonInfluence
-}
-
 extension GameViewController {
+    
+    func setupLevel(){
+        resetLevel()
+        print("level", level)
+        
+        switch level {
+        case 1 : setupLevelOne()
+        case 2 : setupLevelThree()
+        default : setupLevelTwo()
+        }
+        black.fadeOut()
+    }
+    
+    func resetLevel(){
+        level = level > 3 ? 1 : level
+        for child in rootNode.childNodes{
+            print(child.name)
+            if child.name != "camera" && child.name != "staticLight"{
+                child.removeFromParentNode()
+            }
+        }
+        rootNode.addChildNode(black)
+    }
     
     func nextLevel(){
         print(#function)
@@ -30,61 +47,4 @@ extension GameViewController {
         }
     }
     
-    func resetLevel(){
-        level = level > 3 ? 1 : level
-        for child in rootNode.childNodes{
-            child.removeFromParentNode()
-        }
-        rootNode.addChildNode(black)
-    }
-    
-    func setupLevel(){
-        resetLevel()
-        print("level", level)
-        
-        switch level {
-        case 1 : setupLevelOne()
-        case 2 : setupLevelThree()
-        default : setupLevelTwo()
-        }
-        black.fadeOut()
-    }
-    
-    
-    func gearSystem(gearA : Gear, gearB : Gear, relation : relation, hitResult : SCNHitTestResult, translation : CGPoint){
-        gearB.isHaptic = !gearA.isHaptic
-        gearB.isRotating = false
-        
-        let negativeTranslation = CGPoint(x: -translation.x, y: translation.y)
-        
-        if gearA.isRotating && !gearB.isRotating{
-            gearA.rotateGear(from : hitResult, by: translation)
-            switch relation {
-            case .oppositeDirection : gearB.rotateGear(from: hitResult, by: negativeTranslation)
-            case .sameDirection : gearB.rotateGear(from: hitResult, by: translation)
-            default : break
-            }
-        }
-    }
-    
-    func halfGearSystem(gear : Gear, halfGear : HalfGear, hitResult : SCNHitTestResult, translation : CGPoint){
-        let negativeTranslation = CGPoint(x: -translation.x, y: translation.y)
-        
-        if halfGear.isRotateAble{
-            halfGear.rotateGear(from: hitResult, by: negativeTranslation)
-            gear.rotateGear(from : hitResult, by: translation)
-        }
-    }
-    
-    func platformSystem(gear : Gear, platform : Platform, relation : relation, hitResult : SCNHitTestResult, translation : CGPoint){
-        
-        let negativeTranslation = CGPoint(x: -translation.x, y: translation.y)
-        
-        gear.rotateGear(from : hitResult, by: translation)
-        switch relation {
-        case .oppositeDirection : platform.move(by : negativeTranslation)
-        case .sameDirection : platform.move(by: translation)
-        default : break
-        }
-    }
 }
